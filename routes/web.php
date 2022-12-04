45e4ef66b1644dc5623f8b1fa8592e3c2df2e649
yanokandrey <yanok.rivne@gmail.com>
2022-12-04 01:41:17 +0100

    +Middleware_auth

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,17 @@ Route::get('/logout',[AuthController::class, 'Logout']);
 
 Route::get('/auth/google/callback',[AuthController::class, 'Login']);
 
-Route::get('/dashboard/basicsWelcome',[DashboardController::class, 'basicsWelcome'])->name('dashboard.basicsWelcome');
-Route::post('/dashboard/basicsWelcomePost',[DashboardController::class, 'basicsWelcomePost'])->name('dashboard.basicsWelcomePost');
+Route::group(['prefix' =>'dashboard', 'middleware' => 'auth'], function(){
+	Route::get('/basicsWelcome',[DashboardController::class, 'basicsWelcome'])->name('dashboard.basicsWelcome');
+	Route::post('/basicsWelcomePost',[DashboardController::class, 'basicsWelcomePost'])->name('dashboard.basicsWelcomePost');
 
-Route::get('/dashboard/basicsSEO',[DashboardController::class, 'basicsSEO'])->name('dashboard.basicsSEO');
+	Route::get('/basicsSEO',[DashboardController::class, 'basicsSEO'])->name('dashboard.basicsSEO');
 
-Route::get('/dashboard/steps',[DashboardController::class, 'steps'])->name('dashboard.steps');
-Route::get('/dashboard/package',[DashboardController::class, 'package'])->name('dashboard.package');
-Route::get('/dashboard/delivery',[DashboardController::class, 'delivery'])->name('dashboard.delivery');
-Route::get('/dashboard/payments',[DashboardController::class, 'payments'])->name('dashboard.payments');
-
-
+	Route::get('/steps',[DashboardController::class, 'steps'])->name('dashboard.steps');
+	Route::get('/package',[DashboardController::class, 'package'])->name('dashboard.package');
+	Route::get('/delivery',[DashboardController::class, 'delivery'])->name('dashboard.delivery');
+	Route::get('/payments',[DashboardController::class, 'payments'])->name('dashboard.payments');
+});
 Route::get('/order/order',[OrderController::class, 'order'])->name('order.order');
 
 
