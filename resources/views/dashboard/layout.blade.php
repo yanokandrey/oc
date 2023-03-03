423e2fde1c4fbe4fcb299b8715080a312aaf9455
yanokandrey <yanok.rivne@gmail.com>
2023-03-03 18:46:33 +0100

    Hints

--- resources/views/dashboard/layout.blade.php
+++ resources/views/dashboard/layout.blade.php
@@ -20,7 +20,7 @@
 			<div class='col-lg-2 col-md-3 col-sm-12'>
 			@yield('sidebar')
 			</div>
-			<div class='col-lg-7 col-md-5 col-sm-12 h-100 '>
+			<div class='col-lg-7 col-md-5 col-sm-12 h-100'>
 			@yield('main')
 			</div>
 			<div class='col-lg-3 col-md-4 col-sm-12'>
@@ -33,12 +33,10 @@
 			</div>
 		</div>
 		@endauth
-			
 		@guest
 			@yield('guest')
 		@endguest
 	</div>
-
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
